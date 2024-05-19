5b1760f78e43d25bc45b07967c6cfdcbbfcbbdc4
Ba-Han <96304699+Ba-Han@users.noreply.github.com>
2024-05-19 13:44:38 +0630

    RESTApi

---
+++ KhunBaHan.SampleRestApi/Controllers/UserController.cs
@@ -0,0 +1,44 @@
+﻿using KhunBaHan.SampleRestApi.Models;
+using KhunBaHan.SampleRestApi.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace KhunBaHan.SampleRestApi.Controllers
+{
+    public class UserController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly AdoDotNetServices _adoDotNetServices;
+
+        public UserController(IConfiguration configuration, AdoDotNetServices adoDotNetServices)
+        {
+            _configuration = configuration;
+            _adoDotNetServices = adoDotNetServices;
+        }
+
+        [HttpPost]
+        [Route("api/account/login")]
+        public IActionResult Login([FromBody] LoginRequestModel loginRequest)
+        {
+            string query = @"SELECT [Id]
+                  ,[UserName]
+                  ,[Email]
+                  ,[Password]
+                  ,[IsActive]
+              FROM [dbo].[userLoing] WHERE Email = @Email AND Password = @Password AND IsActive = @IsActive";
+            List<SqlParameter> para = new List<SqlParameter>()
+           {
+               new SqlParameter("@Email", loginRequest.Email),
+               new SqlParameter("@Password", loginRequest.Password),
+               new SqlParameter("@IsActive", true)
+           };
+
+            DataTable dt = _adoDotNetServices.QueryFirstOrDefatul(query, para.ToArray());
+            string message = dt.Rows.Count > 0 ? "Login Successful." : "Login Fail.";
+
+            return Ok(message);
+        }
+    }
+}
---
+++ KhunBaHan.SampleRestApi/Services/AdoDotNetServices.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace KhunBaHan.SampleRestApi.Services
+{
+    public class AdoDotNetServices
+    {
+        public readonly IConfiguration _configuration;
+
+        public AdoDotNetServices(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public List<T> Query<T>(string query, SqlParameter[] parameters = null!)
+        {
+            SqlConnection conn = new SqlConnection(_configuration?.GetConnectionString("DefaultConnection"));
+            conn.Open();
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddRange(parameters);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            conn.Close();
+
+            string jsonStr = JsonConvert.SerializeObject(dt);
+            List<T> resStr = JsonConvert.DeserializeObject<List<T>>(jsonStr)!;
+
+            return resStr;
+        }
+
+        public DataTable QueryFirstOrDefatul(string query, SqlParameter[] parameters = null!)
+        {
+            SqlConnection conn = new SqlConnection(_configuration?.GetConnectionString("DefaultConnection"));
+            conn.Open();
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddRange(parameters);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            conn.Close();
+
+            return dt;
+        }
+
+        public int Execute (string query, SqlParameter[] parameters = null!)
+        {
+            SqlConnection conn = new SqlConnection(_configuration?.GetConnectionString("DefaultConnection"));
+            conn.Open();
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddRange(parameters);
+            int resExecute = cmd.ExecuteNonQuery();
+            conn.Close();
+
+            return resExecute;
+        }
+    }
+}
