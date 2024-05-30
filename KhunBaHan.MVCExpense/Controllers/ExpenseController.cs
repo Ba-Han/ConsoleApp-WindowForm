731f3f7c878ba9007626c810fea22d052736067f
Ba-Han <96304699+Ba-Han@users.noreply.github.com>
2024-05-30 15:42:48 +0630

    MVCExpense

---
+++ KhunBaHan.MVCExpense/Controllers/ExpenseController.cs
@@ -0,0 +1,60 @@
+﻿using KhunBaHan.MVCExpense.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KhunBaHan.MVCExpense.Controllers
+{
+    public class ExpenseController : Controller
+    {
+        private readonly ExpenseDbContext _context;
+
+        public ExpenseController(ExpenseDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Expense()
+        {
+            return View();
+        }
+
+        public IActionResult CreateOrEditExpense(int? Id)
+        {
+            if(Id != null)
+            {
+                var editExpenseLst = _context.Expenses.SingleOrDefault(expense => expense.Id == Id);
+                return View(editExpenseLst);
+            }
+            return View();
+        }
+
+        public IActionResult OverView()
+        {
+            var allExpenseLst = _context.Expenses.ToList();
+            var totalExpense = _context.Expenses.Sum(expense => expense.Value);
+            ViewBag.Expenses = totalExpense;
+            return View(allExpenseLst);
+        }
+
+        public IActionResult DeleteExpense(int? Id)
+        {
+            var deleteExpense = _context.Expenses.SingleOrDefault(expense => expense.Id == Id);
+            _context.Expenses.Remove(deleteExpense);
+            _context.SaveChanges();
+            return RedirectToAction("OverView");
+        }
+
+        public IActionResult CreateOrEditExpenseForm(Expense expense)
+        {
+            if (expense.Id == 0)
+            {
+                _context.Expenses.Add(expense);
+            } else
+            {
+                _context.Expenses.Update(expense);
+            }
+            _context.SaveChanges();
+
+            return RedirectToAction("OverView");
+        }
+    }
+}
---
+++ KhunBaHan.MVCExpense/Models/Expense.cs
@@ -0,0 +1,9 @@
+﻿namespace KhunBaHan.MVCExpense.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        public decimal? Value { get; set; }
+        public string? Description { get; set; }
+    }
+}
